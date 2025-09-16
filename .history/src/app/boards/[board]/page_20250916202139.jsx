37f520f1884190b9f6b470bea4697873a666c8e0
rimsha-shoukat <rimsha.devs@gmail.com>
2025-09-16 20:38:07 +0500

    theme changed

---
+++ .history/src/app/boards/[board]/page_20250916202139.jsx
@@ -0,0 +1,44 @@
+'use client';
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import { useParams } from 'next/navigation';
+import ListDialogBox from '@/components/listDialogBox';
+import ListArea from '@/components/listArea';
+import CardDialogBox from '@/components/cardDialogBox';
+
+export default function Boards() {
+  const [boardList, setBoardList] = useState([]);
+
+useEffect(() => {
+  const storedBoards = localStorage.getItem('boards') || '[]';
+  setBoardList(JSON.parse(storedBoards));
+}, []);
+    
+useEffect(() => {
+  console.log(boardList);
+  
+}, [boardList]);
+
+  const params = useParams();
+  const boardId = params.board;
+  const board = boardList.find(b => b.id === boardId);
+  const [listBox, setListBox] = useState(false);
+  const [newCard, setNewCard] = useState(false);
+  const [activeList, setActiveList] = useState('');
+
+  return (
+    <>
+      <section className="w-[100%] flex flex-row justify-between items-center bg-[#b32509] py-[0.6rem] px-4 shadow-md select-none">
+        <Link>
+        <h1 className="text-white text-[2rem] font-bold">Trello</h1>
+        </
+        <div className="flex flex-row gap-4">
+          <button onClick={() => setListBox(!listBox)} className="py-2 px-4 bg-gray-400/60 hover:bg-gray-400/40 rounded-md cursor-pointer font-semibold">+ List</button>
+        </div>
+        <ListDialogBox listBox={listBox} setListBox={setListBox} boardId={boardId} boardList={boardList} setBoardList={setBoardList} />
+        {board && <CardDialogBox newCard={newCard} setNewCard={setNewCard} board={board} activeList={activeList} boardList={boardList} setBoardList={setBoardList} />}
+      </section>
+      <ListArea board={board} newCard={newCard} setNewCard={setNewCard} setActiveList={setActiveList} />
+    </>
+  )
+}
---
+++ .history/src/app/page_20250916194251.jsx
@@ -0,0 +1,32 @@
+'use client';
+import { useState, useEffect } from 'react';
+import BoxDialogBox from '@/components/boxDialogBox';
+import BoxArea from '@/components/boxArea';
+
+export default function Home() {
+  const [boardBox, setBoardBox] = useState(false);
+  const [boardList, setBoardList] = useState([]);
+
+  useEffect(() => {
+    const storedBoards = localStorage.getItem('boards') || '[]';
+    setBoardList(JSON.parse(storedBoards));
+  }, []);
+
+  return (
+    <>
+      <section className="w-[100%] flex flex-row justify-between items-center bg-[#D6B5] py-[0.6rem] px-4 shadow-md select-none">
+        <h1 className="text-white text-[2rem] font-bold">Trello</h1>
+        <div className="flex flex-row gap-4">
+          <button
+            onClick={() => setBoardBox(!boardBox)}
+            className="py-2 px-4 bg-[#b32509] hover:shadow-sm hover:scale-105 hover:shadow-gray-300 rounded-md cursor-pointer font-semibold text-white shadow-md transition-all duration-300"
+          >
+            + Board
+          </button>
+        </div>
+        <BoxDialogBox boardBox={boardBox} setBoardBox={setBoardBox} boardList={boardList} setBoardList={setBoardList} />
+      </section>
+      <BoxArea boardList={boardList} setBoardList={setBoardList} />
+    </>
+  );
+}
