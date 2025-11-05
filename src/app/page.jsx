9925b2f5abdcbcb35efb46faf23d4d65eba6ca9f
rimsha-shoukat <rimsha.devs@gmail.com>
2025-11-05 20:46:00 +0500

    section-title, user-icon components added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,17 @@
+
 import { ToggleTheme } from "@/components/toggle-theme";
 import { SearchBar } from "@/components/search-bar";
-
+import { UserIcon } from "@/components/user-icon";
+import { SectionTitle } from "@/components/section-title"
+import { SectionDetail } from "@/components/section-detail"
 export default function Home() {
   return (
-    <>
-    <h1>Hello</h1>
-     <SearchBar/>
-     <ToggleTheme/>
-    </>
+    <div className="flex flex-row items-center justify-between p-3">
+      <SectionTitle />
+      <SectionDetail />
+      <SearchBar />
+      <UserIcon />
+      <ToggleTheme />
+    </div>
   );
 }
---
+++ src/components/user-icon.jsx
@@ -0,0 +1,9 @@
+import { FaRegUser } from "react-icons/fa";
+
+export function UserIcon(){
+    return (
+        <div className="p-2 bg-gray-400 border-1 dark:text-black border-gray-500 rounded-full">
+          <FaRegUser/>
+        </div>   
+    )
+}
