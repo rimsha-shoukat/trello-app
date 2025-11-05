e39b1261e1fa2969696406a402d1ef2b5e70fac1
rimsha-shoukat <rimsha.devs@gmail.com>
2025-11-05 13:54:31 +0500

    search bar added

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,4 +1,6 @@
 
 export function SearchBar(){
-    <input type="text" placeholder="Search"/>
+    return (
+        <input className="w-[60%] h-auto px-4 py-1 rounded-md border-none ring-1 ring-gray-400 hover:scale-[1.02] focus:border-none hover:transition-all duration-500 ease-in-out" type="text" placeholder="Search"/>
+    )
 }
