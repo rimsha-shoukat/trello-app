8c679d65fa2a04d4054f2713d0394eba3944b784
rimsha-shoukat <rimsha.devs@gmail.com>
2025-12-18 19:05:47 +0500

    card disabled attribute added

--- src/components/utils/card.jsx
+++ src/components/utils/card.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { CloudCheck, DiamondMinus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-export function Card({ list, setNotice, boardId }) {
+export function Card({ list, setNotice, boardId, fetchBoards }) {
     const [isEditing, setIsEditing] = useState(null);
 
     const handleCardUpdate = async (cardId) => {
@@ -38,6 +38,7 @@ export function Card({ list, setNotice, boardId }) {
 
         try {
             await axios.patch("/api/user/check-card", { check, cardId, listId: list._id, boardId });
+            fetchBoards();
         } catch (error) {
             let errorMessage = "An unknown error occurred!!";
             if (error.response) {
@@ -59,9 +60,9 @@ export function Card({ list, setNotice, boardId }) {
 
     return (
         list.cards.map((card) => (
-            <div id={card._id} key={card._id} disabled={card.complete} className="w-full h-auto flex flex-row items-center justify-start gap-2 border border-gray-400 rounded-md p-2 bg-gray-400/50 dark:bg-gray-950/50 shadow-sm">
+            <div id={card._id} key={card._id} className={`w-full h-auto flex flex-row items-center justify-start gap-2 border border-gray-400 rounded-md p-2 bg-gray-400/50 dark:bg-gray-950/50 shadow-sm mb-2 ${card.complete ? "opacity-50" : ""}`}>
                 <input onChange={() => handleCheck(card._id, card.complete)} type="checkbox" checked={card.complete} />
-                <textarea rows="1" onClick={() => setIsEditing(card._id)} className={`${card.complete ? "line-through" : ""} text-justify w-full p-2`} defaultValue={card.text || "Null"} />
+                <textarea disabled={card.complete} rows="1" onClick={() => setIsEditing(card._id)} className={`${card.complete ? "line-through" : ""} text-justify w-full p-2`} defaultValue={card.text || "Null"} />
                 {isEditing === card._id ? (
                     <Button onClick={() => handleCardUpdate(card._id)} className="hover:bg-gray-300" variant="ghost"><CloudCheck /></Button>
                 ) : (
